99b8f061ca57adf3a15ba941d424ae3970eabe6a
kliffeup <azatvaleev356778@gmail.com>
2021-11-17 12:09:31 +0300

    added video part

--- main_end2end.py
+++ main_end2end.py
@@ -1,14 +1,15 @@
 """
  # Copyright 2020 Adobe
  # All Rights Reserved.
- 
+
  # NOTICE: Adobe permits you to use, modify, and distribute this file in
  # accordance with the terms of the Adobe license agreement accompanying
  # it.
- 
+
 """
 
 import sys
+
 sys.path.append('thirdparty/AdaptiveWingLoss')
 import os, glob
 import numpy as np
@@ -29,20 +30,26 @@ default_head_name = 'dali'
 ADD_NAIVE_EYE = True
 CLOSE_INPUT_FACE_MOUTH = False
 
-
 parser = argparse.ArgumentParser()
-parser.add_argument('--jpg', type=str, default='{}.jpg'.format(default_head_name))
-parser.add_argument('--close_input_face_mouth', default=CLOSE_INPUT_FACE_MOUTH, action='store_true')
+parser.add_argument('--jpg', type=str,
+                    default='{}.jpg'.format(default_head_name))
+parser.add_argument('--close_input_face_mouth', default=CLOSE_INPUT_FACE_MOUTH,
+                    action='store_true')
 
-parser.add_argument('--load_AUTOVC_name', type=str, default='examples/ckpt/ckpt_autovc.pth')
-parser.add_argument('--load_a2l_G_name', type=str, default='examples/ckpt/ckpt_speaker_branch.pth')
-parser.add_argument('--load_a2l_C_name', type=str, default='examples/ckpt/ckpt_content_branch.pth') #ckpt_audio2landmark_c.pth')
-parser.add_argument('--load_G_name', type=str, default='examples/ckpt/ckpt_116_i2i_comb.pth') #ckpt_image2image.pth') #ckpt_i2i_finetune_150.pth') #c
+parser.add_argument('--load_AUTOVC_name', type=str,
+                    default='examples/ckpt/ckpt_autovc.pth')
+parser.add_argument('--load_a2l_G_name', type=str,
+                    default='examples/ckpt/ckpt_speaker_branch.pth')
+parser.add_argument('--load_a2l_C_name', type=str,
+                    default='examples/ckpt/ckpt_content_branch.pth')  # ckpt_audio2landmark_c.pth')
+parser.add_argument('--load_G_name', type=str,
+                    default='examples/ckpt/ckpt_116_i2i_comb.pth')  # ckpt_image2image.pth') #ckpt_i2i_finetune_150.pth') #c
 
 parser.add_argument('--amp_lip_x', type=float, default=2.)
 parser.add_argument('--amp_lip_y', type=float, default=2.)
 parser.add_argument('--amp_pos', type=float, default=.5)
-parser.add_argument('--reuse_train_emb_list', type=str, nargs='+', default=[]) #  ['iWeklsXc0H8']) #['45hn7-LXDX8']) #['E_kmpT-EfOg']) #'iWeklsXc0H8', '29k8RtSUjE0', '45hn7-LXDX8',
+parser.add_argument('--reuse_train_emb_list', type=str, nargs='+',
+                    default=[])  # ['iWeklsXc0H8']) #['45hn7-LXDX8']) #['E_kmpT-EfOg']) #'iWeklsXc0H8', '29k8RtSUjE0', '45hn7-LXDX8',
 parser.add_argument('--add_audio_in', default=False, action='store_true')
 parser.add_argument('--comb_fan_awing', default=False, action='store_true')
 parser.add_argument('--output_folder', type=str, default='examples')
@@ -59,7 +66,8 @@ parser.add_argument('--init_content_encoder', type=str, default='')
 parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
 parser.add_argument('--reg_lr', type=float, default=1e-6, help='weight decay')
 parser.add_argument('--write', default=False, action='store_true')
-parser.add_argument('--segment_batch_size', type=int, default=1, help='batch size')
+parser.add_argument('--segment_batch_size', type=int, default=1,
+                    help='batch size')
 parser.add_argument('--emb_coef', default=3.0, type=float)
 parser.add_argument('--lambda_laplacian_smooth_loss', default=1.0, type=float)
 parser.add_argument('--use_11spk_only', default=False, action='store_true')
@@ -67,29 +75,29 @@ parser.add_argument('--use_11spk_only', default=False, action='store_true')
 opt_parser = parser.parse_args()
 
 ''' STEP 1: preprocess input single image '''
-img =cv2.imread('examples/' + opt_parser.jpg)
-predictor = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, device='cuda', flip_input=True)
+img = cv2.imread('examples/' + opt_parser.jpg)
+predictor = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D,
+                                         device='cuda', flip_input=True)
 shapes = predictor.get_landmarks(img)
 if (not shapes or len(shapes) != 1):
     print('Cannot detect face landmarks. Exit.')
+    os.system('rm -f ./examples/*.wav')
+    os.system(f'rm -f ./examples/{opt_parser.jpg}')
     exit(-1)
 shape_3d = shapes[0]
 
-if(opt_parser.close_input_face_mouth):
+if (opt_parser.close_input_face_mouth):
     util.close_input_face_mouth(shape_3d)
-
 
 ''' Additional manual adjustment to input face landmarks (slimmer lips and wider eyes) '''
 # shape_3d[48:, 0] = (shape_3d[48:, 0] - np.mean(shape_3d[48:, 0])) * 0.95 + np.mean(shape_3d[48:, 0])
 shape_3d[49:54, 1] += 1.
 shape_3d[55:60, 1] -= 1.
-shape_3d[[37,38,43,44], 1] -=2
-shape_3d[[40,41,46,47], 1] +=2
-
+shape_3d[[37, 38, 43, 44], 1] -= 2
+shape_3d[[40, 41, 46, 47], 1] += 2
 
 ''' STEP 2: normalize face as input to audio branch '''
 shape_3d, scale, shift = util.norm_input_face(shape_3d)
-
 
 ''' STEP 3: Generate audio data as input to audio branch '''
 # audio real data
@@ -99,21 +107,25 @@ ains = glob.glob1('examples', '*.wav')
 ains = [item for item in ains if item is not 'tmp.wav']
 ains.sort()
 for ain in ains:
-    os.system('ffmpeg -y -loglevel error -i examples/{} -ar 16000 examples/tmp.wav'.format(ain))
+    os.system(
+        'ffmpeg -y -loglevel error -i examples/{} -ar 22050 examples/tmp.wav'.format(
+            ain))
     shutil.copyfile('examples/tmp.wav', 'examples/{}'.format(ain))
 
     # au embedding
     from thirdparty.resemblyer_util.speaker_emb import get_spk_emb
+
     me, ae = get_spk_emb('examples/{}'.format(ain))
     au_emb.append(me.reshape(-1))
 
     print('Processing audio file', ain)
     c = AutoVC_mel_Convertor('examples')
 
-    au_data_i = c.convert_single_wav_to_autovc_input(audio_filename=os.path.join('examples', ain),
-           autovc_model_path=opt_parser.load_AUTOVC_name)
+    au_data_i = c.convert_single_wav_to_autovc_input(
+        audio_filename=os.path.join('examples', ain),
+        autovc_model_path=opt_parser.load_AUTOVC_name)
     au_data += au_data_i
-if(os.path.isfile('examples/tmp.wav')):
+if (os.path.isfile('examples/tmp.wav')):
     os.remove('examples/tmp.wav')
 
 # landmark fake placeholder
@@ -127,11 +139,12 @@ for au, info in au_data:
     rot_quat.append(np.zeros(shape=(au_length, 4)))
     anchor_t_shape.append(np.zeros(shape=(au_length, 68 * 3)))
 
-if(os.path.exists(os.path.join('examples', 'dump', 'random_val_fl.pickle'))):
+if (os.path.exists(os.path.join('examples', 'dump', 'random_val_fl.pickle'))):
     os.remove(os.path.join('examples', 'dump', 'random_val_fl.pickle'))
-if(os.path.exists(os.path.join('examples', 'dump', 'random_val_fl_interp.pickle'))):
+if (os.path.exists(
+        os.path.join('examples', 'dump', 'random_val_fl_interp.pickle'))):
     os.remove(os.path.join('examples', 'dump', 'random_val_fl_interp.pickle'))
-if(os.path.exists(os.path.join('examples', 'dump', 'random_val_au.pickle'))):
+if (os.path.exists(os.path.join('examples', 'dump', 'random_val_au.pickle'))):
     os.remove(os.path.join('examples', 'dump', 'random_val_au.pickle'))
 if (os.path.exists(os.path.join('examples', 'dump', 'random_val_gaze.pickle'))):
     os.remove(os.path.join('examples', 'dump', 'random_val_gaze.pickle'))
@@ -140,25 +153,25 @@ with open(os.path.join('examples', 'dump', 'random_val_fl.pickle'), 'wb') as fp:
     pickle.dump(fl_data, fp)
 with open(os.path.join('examples', 'dump', 'random_val_au.pickle'), 'wb') as fp:
     pickle.dump(au_data, fp)
-with open(os.path.join('examples', 'dump', 'random_val_gaze.pickle'), 'wb') as fp:
-    gaze = {'rot_trans':rot_tran, 'rot_quat':rot_quat, 'anchor_t_shape':anchor_t_shape}
+with open(os.path.join('examples', 'dump', 'random_val_gaze.pickle'),
+          'wb') as fp:
+    gaze = {'rot_trans': rot_tran, 'rot_quat': rot_quat,
+            'anchor_t_shape': anchor_t_shape}
     pickle.dump(gaze, fp)
-
 
 ''' STEP 4: RUN audio->landmark network'''
 model = Audio2landmark_model(opt_parser, jpg_shape=shape_3d)
-if(len(opt_parser.reuse_train_emb_list) == 0):
+if (len(opt_parser.reuse_train_emb_list) == 0):
     model.test(au_emb=au_emb)
 else:
     model.test(au_emb=None)
-
 
 ''' STEP 5: de-normalize the output to the original image scale '''
 fls = glob.glob1('examples', 'pred_fls_*.txt')
 fls.sort()
 
-for i in range(0,len(fls)):
-    fl = np.loadtxt(os.path.join('examples', fls[i])).reshape((-1, 68,3))
+for i in range(0, len(fls)):
+    fl = np.loadtxt(os.path.join('examples', fls[i])).reshape((-1, 68, 3))
     fl[:, :, 0:2] = -fl[:, :, 0:2]
     fl[:, :, 0:2] = fl[:, :, 0:2] / scale - shift
 
@@ -168,12 +181,13 @@ for i in range(0,len(fls)):
     # additional smooth
     fl = fl.reshape((-1, 204))
     fl[:, :48 * 3] = savgol_filter(fl[:, :48 * 3], 15, 3, axis=0)
-    fl[:, 48*3:] = savgol_filter(fl[:, 48*3:], 5, 3, axis=0)
+    fl[:, 48 * 3:] = savgol_filter(fl[:, 48 * 3:], 5, 3, axis=0)
     fl = fl.reshape((-1, 68, 3))
 
     ''' STEP 6: Imag2image translation '''
     model = Image_translation_block(opt_parser, single_test=True)
     with torch.no_grad():
-        model.single_test(jpg=img, fls=fl, filename=fls[i], prefix=opt_parser.jpg.split('.')[0])
+        model.single_test(jpg=img, fls=fl, filename=fls[i],
+                          prefix=opt_parser.jpg.split('.')[0])
         print('finish image2image gen')
     os.remove(os.path.join('examples', fls[i]))
